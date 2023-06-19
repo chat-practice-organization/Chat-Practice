1370ea4f75d46bdd905dc614c9f45ee156b4700e
lsjpjs1 <lsjpjs1@naver.com>
2023-06-19 18:39:07 +0900

    feat: jpa 디비 연동

---
+++ src/main/java/com/practice/chat/config/JpaConfig.java
@@ -0,0 +1,20 @@
+package com.practice.chat.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.keyvalue.core.KeyValueTemplate;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.core.RedisKeyValueTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.mapping.RedisMappingContext;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.practice.chat.repository.jpa")
+public class JpaConfig {}
