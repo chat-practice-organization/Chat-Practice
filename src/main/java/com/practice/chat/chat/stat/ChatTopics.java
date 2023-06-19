117c2e5960a6def1e9f51f1499a5246614dc2dc0
lsjpjs1 <lsjpjs1@naver.com>
2023-06-19 18:42:36 +0900

    feat: 카프카 토픽 관련 상수 클래스 추가

---
+++ src/main/java/com/practice/chat/chat/stat/ChatTopics.java
@@ -0,0 +1,11 @@
+package com.practice.chat.chat.stat;
+
+import org.springframework.stereotype.Component;
+
+
+public class ChatTopics {
+
+    public static final String CHAT_MESSAGES_TOPIC = "test_topic";
+
+
+}
