1fbaae7ee8b1f722a817271b1d2f97f4c5d897a9
lsjpjs1 <lsjpjs1@naver.com>
2023-06-19 18:39:50 +0900

    fix: 패키지명 변경

--- src/test/java/com/practice/chat/ChatApplicationTests.java
+++ src/test/java/com/practice/chat/ChatApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.chat;
+package com.practice.chat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- src/test/java/com/practice/chat/chat/service/SendMessageServiceTest.java
+++ src/test/java/com/practice/chat/chat/service/SendMessageServiceTest.java
@@ -1,15 +1,14 @@
-package com.example.chat.chat.service;
+package com.practice.chat.chat.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class SendMessageServiceTest {
 
     @Autowired
-    private SendMessageService sendMessageService;
+    private InsertQueueService insertQueueService;
 
 
     @Test
