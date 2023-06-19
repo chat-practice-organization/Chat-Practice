0c169eb38d4f4d569503388a1ab547d7b06898a6
lsjpjs1 <lsjpjs1@naver.com>
2023-06-19 18:41:13 +0900

    feat: 카프카 설정

---
+++ src/main/java/com/practice/chat/config/KafkaConfig.java
@@ -0,0 +1,8 @@
+package com.practice.chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {}
