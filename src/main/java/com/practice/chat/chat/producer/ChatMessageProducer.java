d2b3621b4827477967955c28895a36f335932f2d
lsjpjs1 <lsjpjs1@naver.com>
2023-07-21 08:22:37 +0900

    feat: Producing key에 채팅방 번호 추가

--- src/main/java/com/practice/chat/chat/producer/ChatMessageProducer.java
+++ src/main/java/com/practice/chat/chat/producer/ChatMessageProducer.java
@@ -17,7 +17,7 @@ public class ChatMessageProducer {
     // 채팅 메시지 메시지 큐에 삽입
     public void produceChatMessage(String topic, ChatMessage chatMessage) {
         try {
-            kafkaTemplate.send(topic, objectMapper.writeValueAsString(chatMessage));
+            kafkaTemplate.send(topic, chatMessage.getChatRoomId(),objectMapper.writeValueAsString(chatMessage));
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
