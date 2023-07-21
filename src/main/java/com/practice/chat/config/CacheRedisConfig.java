01646c23cd68200d9c6c51e60c6fc9ad84ed52df
lsjpjs1 <lsjpjs1@naver.com>
2023-07-21 11:20:15 +0900

    feat: 레디스 캐시 설정 추가

--- src/main/java/com/practice/chat/config/CacheRedisConfig.java
+++ src/main/java/com/practice/chat/config/CacheRedisConfig.java
@@ -1,24 +1,21 @@
 package com.practice.chat.config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.keyvalue.core.KeyValueTemplate;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisKeyValueAdapter;
-import org.springframework.data.redis.core.RedisKeyValueTemplate;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.mapping.RedisMappingContext;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableRedisRepositories(basePackages = "com.practice.chat.repository.redis.cache",
-        redisTemplateRef = "cacheRedisTemplate", keyValueTemplateRef = "cacheKeyValueTemplate",
-        enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
+@EnableCaching
 public class CacheRedisConfig {
     @Value("${redis.cache.host}")
     private String CACHE_HOST;
@@ -26,27 +23,29 @@ public class CacheRedisConfig {
     @Value("${redis.cache.port}")
     private int CACHE_PORT;
 
-
+    //캐시 설정 시 이 빈이 주입될 수 있도록 @Primary 처리해주었습니다.
+    @Primary
     @Bean(name = "cacheRedisFactory")
     public RedisConnectionFactory cacheRedisFactory() {
         return new LettuceConnectionFactory(new RedisStandaloneConfiguration(CACHE_HOST, CACHE_PORT));
     }
 
-    @Bean(name = "cacheKeyValueTemplate")
-    public KeyValueTemplate cacheKeyValueTemplate() {
-        RedisKeyValueTemplate keyValueTemplate = new RedisKeyValueTemplate(
-                new RedisKeyValueAdapter(cacheRedisTemplate()),
-                new RedisMappingContext()
-        );
-        return keyValueTemplate;
-    }
 
-    @Bean(name = "cacheRedisTemplate")
-    public RedisTemplate<String, Object> cacheRedisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(cacheRedisFactory());
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-        return template;
+    // 캐시할 때는 value에 GenericJackson2JsonRedisSerializer를 사용하기 위해 설정해주었습니다.
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager
+                .RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
     }
 }
