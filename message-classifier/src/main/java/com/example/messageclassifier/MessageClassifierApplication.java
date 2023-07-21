09c29e4bc7cb9737acef92bee627f89f0dbe3b59
lsjpjs1 <lsjpjs1@naver.com>
2023-07-21 21:16:31 +0900

    feat: Message classifier initial commit

---
+++ message-classifier/src/main/java/com/example/messageclassifier/MessageClassifierApplication.java
@@ -0,0 +1,13 @@
+package com.example.messageclassifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageClassifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageClassifierApplication.class, args);
+    }
+
+}
---
+++ message-classifier/src/test/java/com/example/messageclassifier/MessageClassifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messageclassifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageClassifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
