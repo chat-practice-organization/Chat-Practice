b37b9eadfeaa0d60c8c08a9c9480c3725fd414b0
lsjpjs1 <lsjpjs1@naver.com>
2023-07-22 12:25:36 +0900

    feat: 컨슈밍 개수 프린트 하는 스케줄러 추가

---
+++ src/main/java/com/practice/chat/chat/consumer/PrintCounterScheduler.java
@@ -0,0 +1,16 @@
+package com.practice.chat.chat.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@EnableScheduling
+@Component
+@Slf4j
+public class PrintCounterScheduler {
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void printConsumerCount() {
+        log.info("consuming: "+ ReceivedChatMessageConsumer.count);
+    }
+}
