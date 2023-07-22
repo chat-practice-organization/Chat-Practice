5d225214d3aed30edbea1bcd0732d1e87c9a036a
lsjpjs1 <lsjpjs1@naver.com>
2023-07-22 22:28:26 +0900

    feat: 공통 모듈 설정 파일 컴포넌트 스캔 설정 추가

---
+++ chat-application/src/main/java/com/practice/chat/config/ComponentScanConfig.java
@@ -0,0 +1,9 @@
+package com.practice.chat.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"config"})
+public class ComponentScanConfig {
+}
---
+++ message-classifier/src/main/java/com/example/messageclassifier/config/ComponentScanConfig.java
@@ -0,0 +1,9 @@
+package com.example.messageclassifier.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"config"})
+public class ComponentScanConfig {
+}
