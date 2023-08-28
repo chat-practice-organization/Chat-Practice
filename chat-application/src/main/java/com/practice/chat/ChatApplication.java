42e41fb00ba45ce84af06af1903566003e1cc7b3
Yu Kyung Hwan <dbrudghks333@naver.com>
2023-08-28 20:06:48 +0900

    feat: elastic APM 적용

--- chat-application/src/main/java/com/practice/chat/ChatApplication.java
+++ chat-application/src/main/java/com/practice/chat/ChatApplication.java
@@ -3,12 +3,13 @@ package com.practice.chat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import co.elastic.apm.attach.ElasticApmAttacher;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ChatApplication {
 
     public static void main(String[] args) {
+        ElasticApmAttacher.attach();
         SpringApplication.run(ChatApplication.class, args);
     }
-
 }
