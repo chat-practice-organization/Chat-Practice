d114d25932677c16226fe44b00a7d95b3a76cac3
lsjpjs1 <lsjpjs1@naver.com>
2023-08-30 00:00:55 +0900

    feat: 메모리 로그 추가

--- message-classifier/src/main/java/com/example/messageclassifier/MessageClassifierApplication.java
+++ message-classifier/src/main/java/com/example/messageclassifier/MessageClassifierApplication.java
@@ -1,15 +1,40 @@
 package com.example.messageclassifier;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import co.elastic.apm.attach.ElasticApmAttacher;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 @SpringBootApplication
 public class MessageClassifierApplication {
+    private static final Logger logger = LoggerFactory.getLogger(MessageClassifierApplication.class);
 
     public static void main(String[] args) {
         ElasticApmAttacher.attach();
         SpringApplication.run(MessageClassifierApplication.class, args);
+        startMemoryLogging();
+
     }
 
+    private static void startMemoryLogging() {
+        Timer timer = new Timer(true);
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                Runtime runtime = Runtime.getRuntime();
+                long usedMemory = (runtime.totalMemory() - runtime.freeMemory()) / 1024 / 1024;
+                long maxMemory = runtime.maxMemory() / 1024 / 1024;
+
+                logger.info("Used memory: {} MB, Max memory: {} MB", usedMemory, maxMemory);
+            }
+        }, 0, 500);
+    }
 }
